3ecd57fe8941724936b5c4ddfc11b99fe3351e1a
Ryan Heimbuch <rheimbuch@gmail.com>
2011-04-13 10:05:44 -0600

    update experimental rack middleware to use newer api

--- lib/rack/rql.rb
+++ lib/rack/rql.rb
@@ -1,16 +1,15 @@
-require 'rql/parser'
+require 'rql'
 
 module Rack
   class RqlQuery
     def initialize(app, js_runtime=nil)
       @app = app
-      @parser = Rql::Parser.new(js_runtime)
     end
 
     def call(env)
       if !env['rql.query'] && env['QUERY_STRING']
         begin
-          env['rql.query'] = @parser.parseQuery(env['QUERY_STRING'])
+          env['rql.query'] = Rql[env['QUERY_STRING']]
         rescue => e
           env['rql.error'] = e
         end
