e85cf1cda3e80621e86cbb01ab1cb7f7b5c1c116
Ryan Heimbuch <rheimbuch@gmail.com>
2011-04-18 13:54:05 -0600

    log rql query errors

--- lib/responders/rql.rb
+++ lib/responders/rql.rb
@@ -3,7 +3,9 @@ module Responders
     def resource
       super_resource = super
       rql = request.env['rql.query']
+      rql_error = rquest.env['rql.error']
       controller.logger.debug("RQL Query: #{rql.inspect}")
+      controller.logger.debug("RQL Error: #{rql_error}")
       if(rql)
         begin
           rql.on(super_resource)
